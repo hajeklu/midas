80a89501565f050850b9e1bca135996efdf90d8e
hajeklu <hajeklu@outlook.com>
2020-12-07 18:18:00 +0100

    Initial project structure.

---
+++ src/main/java/cz/lhhd/midas/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package cz.lhhd.midas.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Hello World.";
+    }
+
+}
