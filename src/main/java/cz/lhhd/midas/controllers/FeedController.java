c3dd5543aa56b99c684f54318c69a36384de9527
hajeklu <hajeklu@outlook.com>
2021-01-02 19:43:32 +0100

    Feed, Cross

--- src/main/java/cz/lhhd/midas/controllers/FeedController.java
+++ src/main/java/cz/lhhd/midas/controllers/FeedController.java
@@ -3,6 +3,7 @@ package cz.lhhd.midas.controllers;
 import cz.lhhd.midas.model.Feed;
 import cz.lhhd.midas.repository.FeedRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,13 @@ public class FeedController {
     @RequestMapping("/api/feed")
     public List<Feed> getAllFeed(){
         return feedRepository.findAll();
+    }
+
+    @ResponseBody
+    @PostMapping("/api/feed/add")
+    public Feed addFeed(Feed feed){
+        feedRepository.save(feed);
+        return feed;
     }
 
 }
